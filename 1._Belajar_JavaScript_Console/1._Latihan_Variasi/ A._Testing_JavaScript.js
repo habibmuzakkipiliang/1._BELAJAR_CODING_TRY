0c1ecf0bd6a9db6fc94d77940ad1cd17846655cb
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2026-03-15 21:54:56 +0700

    Add files via upload

---
+++ 1._Belajar_JavaScript_Console/1._Latihan_Variasi/ A._Testing_JavaScript.js
@@ -0,0 +1,23 @@
+// Tes Program JavaScript Sederhana
+
+
+console.log ("Hello World \n")
+
+
+var selamat = "Selamat kembali Habib, di Windows 11 Pro yang sudah diperbaiki mandiri di rumah selama bulan puasa 2026 (HP Elitebook 8440p, RAM 8 GB, SSD Samsung 496 GB)\n"
+
+console.log (selamat)
+
+
+
+
+var tes = "Semangat, untuk ujian akhir sekolah dan belajar IT nya \n"
+
+console.log (tes)
+
+
+
+
+var tes_1 = "Semangat belajar Coding Python dan JavaScript (HTML dan CSS) \n"
+
+console.log (tes_1)
