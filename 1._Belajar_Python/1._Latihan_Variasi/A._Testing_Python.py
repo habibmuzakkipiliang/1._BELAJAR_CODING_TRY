e81bd6d599fcaddf186780387f0c018668267bfd
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2026-03-15 21:55:58 +0700

    Add files via upload

---
+++ 1._Belajar_Python/1._Latihan_Variasi/A._Testing_Python.py
@@ -0,0 +1,23 @@
+# Tes Program Python Sederhana
+
+
+print ("Hello World \n")
+
+
+selamat = "Selamat kembali Habib, di Windows 11 Pro yang sudah diperbaiki mandiri di rumah selama bulan puasa 2026 (HP Elitebook 8440p, RAM 8 GB, SSD Samsung 496 GB \n"
+
+print (selamat)
+
+
+
+
+tes = "Semangat, untuk ujian akhir sekolah dan belajar IT nya \n"
+
+print (tes)
+
+
+
+
+tes_1 = "Semangat belajar Coding Python dan JavaScript (HTML dan CSS) \n"
+
+print (tes_1)
