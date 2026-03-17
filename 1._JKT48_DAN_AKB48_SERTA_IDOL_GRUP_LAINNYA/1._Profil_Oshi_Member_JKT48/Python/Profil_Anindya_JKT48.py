fec49d5f244a20401151902aafa30f642bc4360e
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2026-03-17 18:32:17 +0700

    Add files via upload

---
+++ 1._JKT48_DAN_AKB48_SERTA_IDOL_GRUP_LAINNYA/1._Profil_Oshi_Member_JKT48/Python/Profil_Anindya_JKT48.py
@@ -0,0 +1,36 @@
+# Profil Anindya JKT48
+
+print ("\n Profil Anindya JKT48 Generasi 11 \n")
+
+
+data = {
+    
+    "nama" : "Anindya Ramadhani Purnomo",
+    "panggil" : "Anindya JKT48",
+    "asal" : "Depok, Jawa Barat",
+    "tanggal_lahir" : "18 Oktober 2005",
+    "gen" : "11 JKT48",
+    "zodiak" : "Libra",
+    "tinggi_badan" : "154 cm",
+    "status_jkt48" : "Member Inti JKT48",
+    "jikoshoukai" : "Si mungil hadir semanis mangga. Hai, aku Anindya!",
+}
+
+
+print ("Nama :", data ["nama"])
+
+print ("Panggil :", data ["panggil"])
+
+print ("Asal :", data ["asal"])
+
+print ("Tanggal lahir :", data ["tanggal_lahir"])
+
+print ("Gen :", data ["gen"])
+
+print ("Zodiak :", data ["zodiak"])
+
+print ("Tinggi badan :", data ["tinggi_badan"])
+
+print ("Status JKT48 :", data ["status_jkt48"])
+
+print ("Jikoshoukai :", data ["jikoshoukai"])
