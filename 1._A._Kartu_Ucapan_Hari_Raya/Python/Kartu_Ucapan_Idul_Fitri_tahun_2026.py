689ce0e33bf2786e4f957fda6462720fa81f5780
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2026-03-22 12:58:01 +0700

    Add files via upload

---
+++ 1._A._Kartu_Ucapan_Hari_Raya/Python/Kartu_Ucapan_Idul_Fitri_tahun_2026.py
@@ -0,0 +1,32 @@
+# Program Kartu Ucapan Spesial Idul Fitri oleh Coding Python
+
+print ("Program Kartu Ucapan Spesial Idul Fitri oleh Coding Python \n")
+
+
+raya = "Hari Raya Idul Fitri"
+hijriah = "1447 Hijriah"
+masehi = "2026 Masehi"
+nama = "Habib Muzakki"
+panggil = "Habib"
+kelas = "12 Agama"
+sekolah = "MAN 2 KOTA SERANG"
+asal = "Kota Serang, Provinsi Banten"
+fans = "JKT48"
+oshi = "Michie, Fritzy, Anindya, Christy JKT48"
+
+
+ucapan = f"""Selamat {raya} {hijriah}, dan tahun {masehi}, minal aidin wal faizin, mohon maaf lahir dan batin,  
+
+Salam dari : 
+- Nama    : {nama} 
+- Panggil : {panggil}
+- Kelas   : {kelas}
+- Sekolah : {sekolah}
+- Asal    : {asal} 
+- Fans    : {fans} 
+- Oshi    : {oshi} 
+
+"""
+
+
+print (ucapan)
