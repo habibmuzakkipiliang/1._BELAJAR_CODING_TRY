2cadfe08e7ddaf4a3833cd3a3c8b412aee3838af
Habib Muzakki Piliang <160248449+habibmuzakkipiliang@users.noreply.github.com>
2026-03-29 11:26:47 +0700

    Add files via upload

---
+++ 1._A._Latihan_Random_Coding/Coding_JavaScript/Latihan_Coding_JavaScript.js
@@ -0,0 +1,679 @@
+// Latihan Coding JavaScript
+
+console.log ("\n Latihan Coding JavaScript \n")
+
+
+console.log ("Hello World")
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+console.log ("\n Sintaks dasar \n")
+
+
+var nama = "Jansen Rust"
+var umur = "25 tahun"
+var tinggi = "170 cm"
+var coding = "HTML, CSS, JavaScript dan Python"
+var warga = "Amerika Serikat"
+var asal = "Silicon Valley"
+
+
+console.log ("Nama :", nama)
+console.log ("Umur :", umur)
+console.log ("Tinggi badan :", tinggi)
+console.log ("Coding :", coding)
+console.log ("Warga :", warga)
+console.log ("Asal :", asal)
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+console.log ("\n Sintaks dasar 2\n")
+
+
+var nama = "Habib Muzakki"
+var marga = "Piliang"
+var kelas = "12 Agama"
+var absen = 15
+var coding = "HTML, CSS, JavaScript dan Python"
+var sekolah = "MAN 2 KOTA SERANG"
+var tinggi = "170 cm"
+
+
+console.log ("Nama :", nama)
+console.log ("Marga :", marga)
+console.log ("Kelas :", kelas)
+console.log ("Absen :", absen)
+console.log ("Coding :", coding)
+console.log ("Sekolah :", sekolah)
+console.log ("Tinggi badan :", tinggi)
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+console.log ("\n Tipe data dasar \n")
+
+
+var teks = "Tes aja"
+var angka = 34
+var desimal = 34.2
+var cek_1 = true
+var cek_2 = false
+var kosong = null
+
+
+console.log ("Teks =", teks)
+console.log ("Angka =", angka)
+console.log ("Desimal =", desimal)
+console.log ("Cek 1 =", cek_1)
+console.log ("Cek 2 =", cek_2)
+console.log ("Kosong =", kosong)
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+console.log ("\n Operasi Dasar \n")
+
+
+var x = 25
+var y = 5
+
+
+console.log ("Tambah =", x + y)
+console.log ("Kurang =", x - y)
+console.log ("Kali =", x * y)
+console.log ("Bagi =", x / y)
+console.log ("Pangkat =", x ** y)
+console.log ("Modulus =", x % y)
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+console.log ("\n Operator Perbandingan \n")
+
+
+console.log ("Hasil =", x > y)
+console.log ("Hasil =", x > y)
+console.log ("Hasil =", x >= y)
+console.log ("Hasil =", x <= y)
+console.log ("Hasil =", x == y)
+console.log ("Hasil =", x != y)
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+console.log ("\n Operator Logika \n")
+
+
+console.log ("Hasil =", (x > y) && (x < y))
+console.log ("Hasil =", (x < y) || (x > y))
+console.log ("Hasil =", (!x))
+console.log ("Hasil =", (!y))
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+console.log ("\n Switch Case \n")
+
+
+var nilai = 95
+
+switch (nilai) {
+    
+    case 95:
+        console.log ("A")
+        break
+      
+    case 90:
+        console.log ("B")
+        break
+         
+    case 80:
+        console.log ("C")
+        break
+        
+    case 75:
+        console.log ("D")
+        break
+        
+    case 70:
+        console.log ("E")
+        break
+    
+    case 60:
+        console.log ("F")
+        break
+        
+    default:
+    console.log ("Sama saja")
+    
+}
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+console.log ("\n Switch Case 2 \n")
+
+
+var kondisi = 2
+
+switch (kondisi) {
+    
+    case 1:
+        console.log ("Iya")
+        break
+        
+    case 2:
+        console.log ("Tidak")
+        break
+        
+    case 3:
+        console.log ("Kadang-kadang")
+        break
+        
+    default:
+    console.log ("Bukan dong")
+    
+}
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+console.log ("\n Percabangan dasar \n")
+
+
+var c = 3
+
+if (c >= 5) {
+    console.log (`Besar, nilai = ${c}`)
+}
+
+else {
+    console.log (`Kecil, nilai = ${c}`)
+}
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+console.log ("\n Percabangan Lanjutan \n")
+
+
+var a = 7
+
+if (a >= 5) {
+    console.log (`Besar, nilai = ${a}`)
+}
+
+else if (a <= 5) {
+    console.log (`Kecil, nilai = ${a}`)
+}
+
+else {
+    console.log (`Sama saja, nilai = ${a}`)
+}
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+console.log ("\n Percabangan Skor Sederhana \n")
+
+
+var b = 5
+
+if (b >= 9) {
+    console.log (`A, nilai = ${b}`)
+}
+
+else if (b >= 8) {
+    console.log (`B, nilai = ${b}`)
+}
+
+else if (b >= 7) {
+    console.log (`C, nilai = ${b}`)
+}
+
+else if (b >= 6) {
+    console.log (`D, nilai = ${b}`)
+}
+
+else if (b >= 5) {
+    console.log (`E, nilai = ${b}`)
+}
+
+else if (b >= 4) {
+    console.log (`F, nilai = ${b}`)
+}
+
+else {
+    console.log (`Sama saja, nilai = ${b}`)
+}
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+console.log ("\n Percabangan Nilai Rapor Sederhana \n")
+
+
+var nilai = 95
+
+if (nilai >= 95) {
+    console.log (`A, nilai = ${nilai}`)
+}
+
+else if (nilai >= 90) {
+    console.log (`B, nilai = ${nilai}`)
+}
+
+else if (nilai >= 80) {
+    console.log (`C, nilai = ${nilai}`)
+}
+
+else if (nilai >= 70) {
+    console.log (`D, nilai = ${nilai}`)
+}
+
+else if (nilai >= 60) {
+    console.log (`E, nilai = ${nilai}`)
+}
+
+else if (nilai >= 50) {
+    console.log (`F, nilai = ${nilai}`)
+}
+
+else {
+    console.log (`Sama saja, nilai = ${nilai}`)
+}
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+console.log ("\n For dasar \n")
+
+
+for (a = 0; a < 11; a++) {
+    console.log (`Urutan ke - ${a}`)
+}
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+for (b = 0; b < 11; b++) {
+    console.log (`Urutan ke- ${b}`)
+}
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+console.log ("\n While dasar \n")
+
+
+var a = 2
+
+while (a < 11) {
+    console.log (`Urutan ke - ${a}`)
+    a++
+}
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+var b = 5
+
+while (b < 16) {
+    console.log (`Urutan ke - ${b}`)
+    b++
+}
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+console.log ("\n Do While dasar \n")
+
+
+var e = 8
+
+do {
+    console.log (`Urutan ke - ${e}`)
+    e++
+} while (e < 17)
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+var h = 3
+
+do {
+    console.log (`Urutan ke - ${h}`)
+    h++
+} while (h < 7)
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+var d = 5
+
+do {
+    console.log (`Urutan ke - ${d}`)
+    d++
+} while (d < 17)
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+console.log ("\n Array \n")
+
+
+var data = [
+    
+    "1. Nangka",
+    "2. Melon",
+    "3. Semangka",
+    "4. Buah Naga",
+    "5. Pepaya",
+    "6. Buah Anggur",
+    
+    ]
+    
+    
+for (a = 0; a < data.length; a++) {
+    console.log (data [a])
+}
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+console.log ("\n Array 2 \n")
+
+
+var daftar = [
+    
+    "1. Rendang",
+    "2. Nasi Padang",
+    "3. Nasi Kapau",
+    "4. Ayam Gulai",
+    "5. Rendang Daging",
+    "6. Sala lauak",
+    
+    ]
+    
+    
+for (b = 0; b < daftar.length; b++) {
+    console.log (daftar [b])
+}
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+console.log ("\n Dictionary \n")
+
+
+var profil = {
+    
+    "nama" : "Jansen Rust",
+    "umur" : "25 tahun",
+    "tinggi" : "170 cm",
+    "coding" : "HTML, CSS, JavaScript, dan Python",
+    "warga" : "Amerika Serikat",
+    "asal" : "Silicon Valley",
+    
+}
+
+
+console.log ("Nama :", profil ["nama"])
+
+console.log ("Umur :", profil ["umur"])
+
+console.log ("Tinggi :", profil ["tinggi"])
+
+console.log ("Coding :", profil ["coding"])
+
+console.log ("Asal :", profil ["asal"])
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+console.log ("\n Dictionary 2 \n")
+
+
+var profil = {
+    
+    "nama" : "Habib Muzakki",
+    "marga" : "Piliang",
+    "kelas" : "12 Agama",
+    "absen" : 15,
+    "coding" : "HTML, CSS, JavaScript, Python",
+    "sekolah" : "MAN 2 KOTA SERANG",
+    "tinggi" : "170 cm",
+    
+}
+
+
+console.log ("Nama :", profil ["nama"])
+
+console.log ("Marga :", profil ["marga"])
+
+console.log ("Kelas :", profil ["kelas"])
+
+console.log ("Absen :", profil ["absen"])
+
+console.log ("Coding :", profil ["coding"])
+
+console.log ("Sekolah :", profil ["sekolah"])
+
+console.log ("Tinggi badan :", profil ["tinggi"])
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+console.log ("\n Percabangan Nested 1 \n")
+
+
+var a = 9
+var cek = true
+
+if (cek) {
+    if (a >= 5) {
+        console.log (`Besar, nilai = ${a}`)
+    }
+    
+    else if (a <= 5) {
+        console.log (`Kecil, nilai = ${a}`)
+    }
+}
+
+else {
+    console.log (`Sama saja, nilai = ${a}`)
+}
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+console.log ("\n Percabangan Nested 2 \n")
+
+
+var b = 3
+var cek = true
+
+if (cek) {
+    if (b >= 5) {
+        console.log (`Besar, nilai = ${b}`)
+    }
+    
+    else {
+        console.log (`Kecil, nilai = ${b}`)
+    }
+}
+
+else {
+    console.log (`Sama saja, nilai = ${b}`)
+}
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+console.log ("\n Percabangan Nested, Daftar JKT48 \n")
+
+
+var usia = 19
+var cek = true
+
+if (cek) {
+    if ((usia >= 13) && (usia <= 19)) {
+        console.log (`Sudah boleh daftar JKT48, usia = ${usia}`)
+    }
+    
+    else if (usia > 19) {
+        console.log (`Sudah matang masuk JKT48, usia = ${usia}`)
+    }
+    
+    else {
+        console.log (`Belum boleh ya, ${usia}`)
+    }
+}
+
+else {
+    console.log (`Jangan dulu ya, usia = ${usia} `)
+}
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+console.log ("\n Percabangan Nested, usia produktif manusia \n")
+
+
+var umur = 25
+var cek = true
+
+if (cek) {
+    if ((umur >= 15) && (umur <= 64)) {
+        console.log (`Masuk usia produktif, umur = ${umur}`)
+    }
+    
+    else if (umur > 64) {
+        console.log (`Masuk lanjut usia, umur = ${umur}`)
+    }
+    
+    else {
+        console.log (`Bukan usia produktif, umur = ${umur}`)
+    }
+}
+
+else {
+    console.log (`Masih kecil usianya, umur = ${umur}`)
+}
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+console.log ("\n For Nested \n")
+
+
+for (a = 0; a < 4; a++) {
+    for (b = 0; b < 4; b++) {
+        console.log (`Urutan ke - ${a}, dan Urutan ke - ${b}`)
+    }
+}
+
+
+console.log ("\n --- Batas --- \n")
+
+
+
+
+console.log ("\n For Nested 1 \n")
+
+
+for (x = 0; x < 4; x++) {
+    for (y = 0; y < 4; y++) {
+        for (z = 0; z < 4; z++) {
+            console.log (`Urutan ke - ${x}, Urutan ke - ${y}, dan Urutan ke - ${z}`)
+        }
+    }
+}
+
+
+console.log ("\n --- Batas --- \n")
---
+++ 1._A._Latihan_Random_Coding/Coding_Python/Latihan_Coding_Python.py
@@ -0,0 +1,261 @@
+# Latihan Coding Python
+
+print ("\n Latihan Coding Python \n")
+
+
+print ("Hello World")
+
+
+print ("\n --- Batas --- \n")
+
+
+
+
+print ("\n Sintaks dasar \n")
+
+
+nama = "Jansen Rust"
+umur = "25 tahun"
+tinggi = "170 cm"
+coding = "HTML, CSS, JavaScript dan Python"
+warga = "Amerika Serikat"
+asal = "Silicon Valley"
+
+
+print ("Nama :", nama)
+print ("Umur :", umur)
+print ("Tinggi :", tinggi)
+print ("Coding :", coding)
+print ("Warga :", warga)
+print ("Asal :", asal)
+
+
+print ("\n --- Batas --- \n")
+
+
+
+
+print ("\n Sintaks dasar 2 \n")
+
+
+nama = "Habib Muzakki"
+marga = "Piliang"
+kelas = "12 Agama"
+absen = 15
+coding = "HTML, CSS, JavaScript dan Python"
+sekolah = "MAN 2 KOTA SERANG"
+tinggi = "170 cm"
+
+
+print ("Nama :", nama)
+print ("Marga :", marga)
+print ("Kelas :", kelas)
+print ("Absen :", absen)
+print ("Coding :", coding)
+print ("Sekolah :", sekolah)
+print ("Tinggi badan :", tinggi)
+
+
+print ("\n --- Batas --- \n")
+
+
+
+
+print ("\n Tipe data dasar \n")
+
+
+teks = "Tes aja"
+angka = 34
+desimal = 34.2
+cek_1 = True
+cek_2 = False
+kosong = None
+
+
+print ("Teks =", teks)
+print ("Angka =", angka)
+print ("Desimal =", desimal)
+print ("Cek 1 =", cek_1)
+print ("Cek 2 =", cek_2)
+print ("Kosong =", kosong)
+
+
+print ("\n --- Batas --- \n")
+
+
+
+
+print ("\n Operator Dasar \n")
+
+
+x = 25
+y = 5
+
+
+print ("Tambah =", x + y)
+print ("Kurang =", x - y)
+print ("Kali =", x * y)
+print ("Bagi =", x / y)
+print ("Pangkat =", x ** y)
+print ("Modulus =", x % y)
+
+
+print ("\n --- Batas --- \n")
+
+
+
+
+print ("\n Operator Perbandingan \n")
+
+
+print ("Hasil =", x > y)
+print ("Hasil =", x > y)
+print ("Hasil =", x >= y)
+print ("Hasil =", x <= y)
+print ("Hasil =", x == y)
+print ("Hasil =", x != y)
+
+
+print ("\n --- Batas --- \n")
+
+
+
+
+print ("\n Operator Logika \n")
+
+
+print ("Hasil =", x > y and x < y)
+print ("Hasil =", x < y or x > y)
+print ("Hasil =", not x)
+print ("Hasil =", not y)
+
+
+print ("\n --- Batas --- \n")
+
+
+
+
+print ("\n Switch Case \n")
+
+
+nilai = 95
+
+match (nilai):
+    
+    case 95:
+        print ("A")
+        
+    case 90:
+        print ("B")
+        
+    case 80:
+        print ("C")
+        
+    case 75:
+        print ("D")
+        
+    case 70:
+        print ("E")
+        
+    case 60:
+        print ("F")
+        
+    case _:
+        print ("Sama saja")
+        
+        
+print ("\n --- Batas --- \n")
+
+
+
+
+print ("\n Switch Case 2 \n")
+
+
+kondisi = 2
+
+match (kondisi):
+    
+    case 1:
+        print ("Iya")
+        
+    case 2:
+        print ("Tidak")
+        
+    case 3:
+        print ("Kadang-kadang")
+        
+    case _:
+        print ("Bukan dong")
+        
+        
+print ("\n --- Batas --- \n")
+
+
+
+
+print ("\n Percabangan Dasar \n")
+
+
+a = 9
+
+if a >= 5:
+    print (f"Besar, nilai = {a}")
+    
+else:
+    print (f"Kecil, nilai = {a}")
+    
+    
+print ("\n --- Batas --- \n")
+
+
+
+
+print ("\n Percabangan Lanjutan \n")
+
+
+b = 3
+
+if b >= 5:
+    print (f"Besar, nilai = {b}")
+    
+elif b <= 5:
+    print (f"Kecil, nilai = {b}")
+    
+else:
+    print (f"Sama sekali, nilai {b}")
+    
+    
+print ("\n --- Batas --- \n")
+
+
+
+
+print ("\n Percabangan Skor Sederhana \n")
+
+
+c = 5 
+
+if c >= 9:
+    print (f"A, nilai = {c}")
+    
+elif c >= 8:
+    print (f"B, nilai = {c}")
+    
+elif c >= 7:
+    print (f"C, nilai = {c}")
+    
+elif c >= 6:
+    print (f"D, nilai = {c}")
+    
+elif c >= 5:
+    print (f"E, nilai = {c}")
+    
+elif c >= 4:
+    print (f"F, nilai = {c}")
+    
+else:
+    print (f"Sama saja, nilai = {c}")
+    
+    
+print ("\n --- Batas --- \n")
